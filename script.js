1268dea16c56ea88be476e0bb1a9c872ea434c45
yeaung00 <yep.aung00@gmail.com>
2022-06-26 16:25:33 -0700

    Added win condition checker

--- script.js
+++ script.js
@@ -3,10 +3,48 @@ const Player = (marker) => {
     return { getMarker };
 }
 
+
 const Gameboard = (() => {
     const p1 = Player('X');
     const p2 = Player('O');
-    
+    let board = ['0', '1', '2',
+                 '3', '4', '5',
+                 '6', '7', '8',
+    ];
+
+    const checkWinCondition = () => {
+        let winCondition = false;
+        let i = 0;
+        let j = 1;
+        while (i <= 6) {
+            if ((board[i] == board[j]) && (board[j] == board[j+1]) && (board[i] == board[j+1])) {
+                winCondition = true;
+            }
+            i += 3;
+            j += 3;
+        }
+        i = 0;
+        while (i < 3) {
+            j = i;
+            if ((board[j] == board[j+3]) && (board[j+3] == board[j+6]) && (board[j] == board[j+6])) {
+                winCondition = true;
+            }
+            i++;
+        }
+        if ((board[0] == board[4]) && (board[4] == board[8]) && (board[0] == board[8])) {
+            winCondition = true;
+        } else if ((board[2] == board[4]) && (board[4] == board[6]) && (board[2] == board[6])) {
+            winCondition = true;
+        }
+
+    }
+
+    const updateBoard = (e) => {
+        let tile = e.target.id;
+        if (turn == 1) { board[tile] = e.target.innerText = p1.getMarker(); }
+        else if (turn == 2) { board[tile] = e.target.innerText = p2.getMarker(); }
+    }
+
     let turn = 2;
     const updateTurn = () => {
         if (turn == 1) {
@@ -28,13 +66,15 @@ const Gameboard = (() => {
     [].forEach.call(tiles, function(item, i) {
         item.addEventListener('click', (e) => {
             checkIfMarked(e);
-            if (isMarked === false) { updateTurn(); }
             if (isMarked === true) {
                 alert("Tile is taken. Please choose another.");
                 return;
             }
+            updateTurn();
             if (turn == 1) { e.target.innerText = p1.getMarker(); }
-            if (turn == 2) { e.target.innerText = p2.getMarker(); }
+            else if (turn == 2) { e.target.innerText = p2.getMarker(); }
+            updateBoard(e);
+            checkWinCondition();
         });
     });
 })();
